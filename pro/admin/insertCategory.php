9ef8ab96a88f655d2e6093a263901c01d3d0f30b
anamshafiq <hafizaanam5@gmail.com>
2019-01-09 23:03:27 -0800

    Class activity 12 Pro

---
+++ pro/admin/insertCategory.php
@@ -0,0 +1,69 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: anam
+ * Date: 2019-01-09
+ * Time: 23:01
+ */
+
+require_once "db_connection.php";
+if(isset($_POST['insert_pro'])){
+    //getting text data from the fields
+    $cat_title = $_POST['cat_title'];
+
+
+
+
+    $insert_catagory = "insert into categories (cat_title) 
+                  VALUES ('$cat_title');";
+    $insert_cati = mysqli_query($con, $insert_catagory);
+    if($insert_cati){
+        header("location: ".$_SERVER['PHP_SELF']);
+    }
+}
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Insert Catagory</title>
+    <meta name="viewport" content="width=device-width,initial-scale=1.0">
+    <link rel="stylesheet" href="../css/bootstrap.css">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Bangers|Old+Standard+TT">
+    <style>
+        * {
+            font-family: 'Old Standard TT', serif;
+        }
+    </style>
+</head>
+<body>
+<div class="container">
+    <h1 class="text-center my-4"><i class="fas fa-plus fa-md"></i> <span class="d-none d-sm-inline"> Add New </span> Product </h1>
+    <form action="insert_catagory.php" method="post" enctype="multipart/form-data">
+        <div class="row">
+            <div class="d-none d-sm-block col-sm-3 col-md-4 col-lg-2 col-xl-2 mt-auto">
+                <label for="pro_title" class="float-md-right"> <span class="d-sm-none d-md-inline"> Catagory </span> Title:</label>
+            </div>
+            <div class="col-sm-9 col-md-8 col-lg-4 col-xl-4">
+                <div class="input-group">
+                    <div class="input-group-prepend">
+                        <div class="input-group-text"><i class="fas fa-file-signature"></i></div>
+                    </div>
+                    <input type="text" class="form-control" id="cat_title" name="cat_title" placeholder="Enter Catagory Title" >
+                </div>
+            </div>
+
+
+        </div>
+
+        <div class="row my-3">
+            <div class="d-none d-sm-block col-sm-3 col-md-4 col-lg-2 col-xl-2 mt-auto"></div>
+            <div class="col-sm-9 col-md-8 col-lg-4 col-xl-4">
+                <button type="submit" name="insert_pro" class="btn btn-primary btn-block"><i class="fas fa-plus"></i> Insert Now </button>
+            </div>
+        </div>
+    </form>
+</div>
+</body>
+</html>
